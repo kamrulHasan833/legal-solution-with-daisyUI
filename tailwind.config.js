2f4221e1ce5f2007b3b07b0334cbef5c1b626f13
Kamrul Hasan <kamrul3496hasan@gmail.com>
2023-07-25 09:01:09 +0600

    Added 3 Secrtion

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          "title-color": '#414040',
+         'decs-color':'#707070',
+         'desc-color-2':'#F3F3F3',
+         'primary-color':'#B68C5A',
+         'link-color':'#CFCFCF'
+        },
+        fontFamily: {
+          poppins: ["Poppins", "sans-seri"],
+          "eb-garamond": ["EB Garamond", "serif"],
+        },
+        maxWidth: {
+          standard: "73.125rem",
+        },
+        backgroundImage: {
+          "hero-bg": "url('./images/banner.png')",
+          "slider-1": "url(./images/client-1.png)",
+          "slider-2": "url(./images/client-2.png)",
+          "slider-3": "url(./images/client-3.png)",
+          "slider-4": "url(./images/client-4.png)",
+        },
+      },
     },
     plugins: [],
   }
